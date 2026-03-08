a2a6b31d22a6aa0f9b0206e63509df7a171338ff
nodahikaru <like365.hondai@gmail.com>
2026-03-09 00:22:46 +0900

    1

--- db/log_repository.py
+++ db/log_repository.py
@@ -3,8 +3,8 @@ from db.database import engine
 
 def access_log(client_ip: str, theme: int):
     query = text("""
-        INSERT INTO logs (ip_address, theme)
-        VALUES (:ip, :theme)
+        INSERT INTO logs (ip_address, theme, log_type)
+        VALUES (:ip, :theme, 1)
     """)
 
     with engine.connect() as conn:
